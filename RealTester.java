8e5af0901bda9850ff79b50666809e19f73a683a
Camila <ccontreras20@stuy.edu>
2020-12-04 23:30:52 -0800

    all functions tested

--- RealTester.java
+++ RealTester.java
@@ -7,5 +7,18 @@ public class RealTester {
     System.out.println(x.multiply(y));
     System.out.println(y.divide(x));
     System.out.println(x.subtract(y));
+
+    RationalNumber z = new RationalNumber(2,4);
+    System.out.println(z.getValue());
+    System.out.println(z.getNumerator());
+    System.out.println(z.getDenominator());
+    System.out.println(z.reciprocal());
+    RationalNumber a = new RationalNumber(1,2);
+    System.out.println(z.equals(a));
+    System.out.println(z.toString());
+    System.out.println(z.multiply(a));
+    System.out.println(z.divide(a));
+    System.out.println(z.add(a));
+    System.out.println(z.subtract(a));
   }
 }
