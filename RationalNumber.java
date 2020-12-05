f7ce7384eec0eefcbcfd6b7f403a600e09521969
Camila <ccontreras20@stuy.edu>
2020-12-04 23:06:40 -0800

    getValue function works

---
+++ RationalNumber.java
@@ -0,0 +1,123 @@
+public class RationalNumber extends Number {
+  private int numerator, denominator;
+  /**Initialize the RationalNumber with the provided values
+  *  if the denominator is 0, make the fraction 0/1 instead
+  *@param nume the numerator
+  *@param deno the denominator
+  */
+  public RationalNumber(int nume, int deno){
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    }
+    else if (deno < 0) {
+      numerator = Math.abs(nume);
+      denominator = Math.abs(deno);
+      reduce();
+    }
+    else {
+      numerator = nume;
+      denominator = deno;
+      reduce();
+    }
+  }
+
+  public double getValue(){
+    return (double) numerator/denominator;
+  }
+
+  /**
+  *@return the numerator
+  */
+  public int getNumerator(){
+    return 0;
+  }
+  /**
+  *@return the denominator
+  */
+  public int getDenominator(){
+    return 0;
+  }
+  /**
+  *@return a new RationalNumber that has the same numerator
+  *and denominator as this RationalNumber but reversed.
+  */
+  public RationalNumber reciprocal(){
+    return null;
+  }
+  /**
+  *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
+  */
+  public boolean equals(RationalNumber other){
+    return false;
+  }
+
+  /**
+  *@return the value expressed as "3/4" or "8/3"
+  */
+  public String toString(){
+    return "0";
+  }
+
+  /**Calculate the GCD of two integers.
+  *@param a the first integers
+  *@param b the second integer
+  *@return the value of the GCD
+  */
+  private static int gcd(int a, int b){
+    if (a==0) {
+      return b;
+    }
+    if (a < b) {
+      int c = a;
+      a = b;
+      b = c;
+    }
+    int y = a % b;
+    while (y != 0) {
+      a = b;
+      b = y;
+      y = a % b;
+    }
+    return b;
+    /*use euclids method or a better one*/
+  //  http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+  }
+  private void reduce(){
+    int g = gcd(Math.abs(numerator), denominator);
+    numerator = numerator / g;
+    denominator = denominator / g;
+  /**
+  *Divide the numerator and denominator by the GCD
+  *This must be used to maintain that all RationalNumbers are
+  *reduced after construction.
+  */
+  }
+  /******************Operations Return a new RationalNumber!!!!****************/
+  /**
+  *Return a new RationalNumber that is the product of this and the other
+  */
+  public RationalNumber multiply(RationalNumber other){
+    return null;
+  }
+
+  /**
+  *Return a new RationalNumber that is the this divided by the other
+  */
+  public RationalNumber divide(RationalNumber other){
+    return null;
+  }
+
+  /**
+  *Return a new RationalNumber that is the sum of this and the other
+  */
+  public RationalNumber add(RationalNumber other){
+    return null;
+  }
+  /**
+  *Return a new RationalNumber that this minus the other
+  */
+  public RationalNumber subtract(RationalNumber other){
+    return null;
+  }
+}
